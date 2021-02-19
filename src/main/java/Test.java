bb6947368ba831001a31e99df96f746f505699b0
jw <825140814@qq.com>
2021-02-19 20:14:46 +0800

    first commit, commit pom.xml

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author jw
+ * @description
+ * @date 2021/2/19 20:13
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
