d7a453deff7d03e6bd4d14f983664a55f8b73504
jw <825140814@qq.com>
2021-02-19 20:32:50 +0800

    hot-fix commit for merge

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,8 @@
  **/
 public class Test {
     public static void main(String[] args) {
+        System.out.println("master second");
+        System.out.println("hot-fix first");
         System.out.println("hot-fix");
     }
 }
