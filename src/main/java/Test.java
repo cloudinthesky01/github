029721d729c7cc733b8eebfc32fb2c85d390893a
cloudinthesky01 <69375216+cloudinthesky01@users.noreply.github.com>
2021-02-19 20:51:52 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -10,5 +10,6 @@ public class Test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push from IDEA");
+        System.out.println("test from github");
     }
 }
